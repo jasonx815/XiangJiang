272509b18a820defc6991cd197ef5aff56bcb352
YanZhiwei <Yan.Zhiwei@hotmail.com>
2020-08-11 23:44:32 +0800

    init dev-feature

---
+++ XiangJiang/Common/DataSetHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+
+namespace XiangJiang.Common
+{
+    /// <summary>
+    ///     DataSet 帮助类
+    /// </summary>
+    /// 时间：2016-01-05 13:18
+    /// 备注：
+    public static class DataSetHelper
+    {
+        #region Methods
+
+        /// <summary>
+        ///     判断DataSet是否是NULL或者没有DataTable
+        /// </summary>
+        /// <param name="dataSet">DataSet</param>
+        /// <returns>是否是NULL或者没有DataTable</returns>
+        /// 时间：2016-01-05 13:19
+        /// 备注：
+        public static bool IsNullOrEmpty(this DataSet dataSet)
+        {
+            return dataSet == null || dataSet.Tables.Count == 0;
+        }
+
+        #endregion Methods
+    }
+}
---
+++ XiangJiang/Model/Validation.cs
@@ -0,0 +1,17 @@
+﻿namespace XiangJiang.Model
+{
+    /// <summary>
+    ///     参数验证实体类
+    /// </summary>
+    public sealed class Validation
+    {
+        #region Properties
+
+        /// <summary>
+        ///     是否可用
+        /// </summary>
+        public bool IsValid { get; set; }
+
+        #endregion Properties
+    }
+}
