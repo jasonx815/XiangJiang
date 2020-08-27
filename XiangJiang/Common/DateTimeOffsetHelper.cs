b06937d48e7819d161e0aa11b40f1aee98ae517b
YanZhiwei <Yan.Zhiwei@hotmail.com>
2020-08-27 23:59:21 +0800

    add

---
+++ XiangJiang/Common/DateTimeOffsetHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace XiangJiang.Common
+{
+    public static class DateTimeOffsetHelper
+    {
+        public static bool IsNullOrEmpty(this DateTimeOffset dateTimeOffset)
+        {
+            return dateTimeOffset == DateTimeOffset.MinValue || dateTimeOffset == DateTimeOffset.MaxValue;
+        }
+
+        public static bool IsNullOrEmpty(this DateTimeOffset? dateTimeOffset)
+        {
+            return dateTimeOffset == null || IsNullOrEmpty(dateTimeOffset.Value);
+        }
+    }
+}
