680285d8a87f982e7ade1169041833e5f3a07ec3
言志伟 <Yan.Zhiwei@hotmail.com>
2020-09-14 18:55:52 +0800

    add function

---
+++ XiangJiang/Common/ExpressionHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace XiangJiang.Common
+{
+    public static class ExpressionHelper
+    {
+        public static string GetMemberName(this Expression expression)
+        {
+            switch (expression.NodeType)
+            {
+                case ExpressionType.MemberAccess:
+                    var memberExpression = (MemberExpression) expression;
+                    var superName = GetMemberName(memberExpression.Expression);
+                    return string.IsNullOrEmpty(superName) ? memberExpression.Member.Name : string.Concat(superName, '.', memberExpression.Member.Name);
+
+                case ExpressionType.Call:
+                    var callExpression = (MethodCallExpression) expression;
+                    return callExpression.Method.Name;
+
+                case ExpressionType.Convert:
+                    var unaryExpression = (UnaryExpression) expression;
+                    return GetMemberName(unaryExpression.Operand);
+
+                case ExpressionType.Parameter:
+                case ExpressionType.Constant:
+                    return string.Empty;
+
+                default:
+                    throw new ArgumentException("The expression is not a member access or method call expression");
+            }
+        }
+    }
+}
