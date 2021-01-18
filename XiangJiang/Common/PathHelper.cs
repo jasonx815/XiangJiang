4f4ffb739d869e747ffaa4570d0c049f092acd14
YanZhiwei <Yan.Zhiwei@hotmail.com>
2021-01-19 00:39:50 +0800

    20210119添加辅助

---
+++ XiangJiang/Common/PathHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace XiangJiang.Common
+{
+    public sealed class PathHelper
+    {
+        public static bool IsSame(string path1, string path2)
+        {
+            if (string.IsNullOrWhiteSpace(path1) || string.IsNullOrWhiteSpace(path2)) return false;
+
+            try
+            {
+                var path3 = Path.GetFullPath(path1);
+                var path4 = Path.GetFullPath(path2);
+                return string.Equals(path3, path4, StringComparison.OrdinalIgnoreCase);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
--- XiangJiang/Common/ProcessHelper.cs
+++ XiangJiang/Common/ProcessHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text;
 using XiangJiang.Core;
 
 namespace XiangJiang.Common
@@ -14,7 +15,7 @@ namespace XiangJiang.Common
                 .CheckFileExists(path);
             var processName = Path.GetFileNameWithoutExtension(path);
             var processes = Process.GetProcessesByName(processName);
-            return processes?.Any() ?? false;
+            return (bool) processes?.Any();
         }
 
         public static bool IsRunning(int id)
@@ -26,6 +27,77 @@ namespace XiangJiang.Common
         {
             var processes = Process.GetProcesses();
             return processes.FirstOrDefault(pr => pr.Id == id);
+        }
+
+        public static void RunCmd(string cmdText, bool runAsAdmin = true, bool useShellExecute = false)
+        {
+            Checker.Begin().NotNullOrEmpty(cmdText, nameof(cmdText));
+            using (var process = new Process())
+            {
+                var startInfo = new ProcessStartInfo
+                {
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    FileName = "cmd.exe",
+                    Arguments = $"/c {cmdText}",
+                    Verb = runAsAdmin ? "runas" : string.Empty,
+                    UseShellExecute = useShellExecute
+                };
+                process.StartInfo = startInfo;
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+                process.WaitForExit();
+            }
+        }
+
+        public static void RunCmd(string cmdText, out string succeedLog, out string failedLog, bool runAsAdmin = true,
+            bool useShellExecute = false)
+        {
+            Checker.Begin().NotNullOrEmpty(cmdText, nameof(cmdText));
+            var succeedOutput = new StringBuilder();
+            var failedOutput = new StringBuilder();
+            using (var process = new Process())
+            {
+                var startInfo = new ProcessStartInfo
+                {
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    FileName = "cmd.exe",
+                    Arguments = $"/c {cmdText}",
+                    Verb = runAsAdmin ? "runas" : string.Empty,
+                    UseShellExecute = useShellExecute,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true
+                };
+                process.OutputDataReceived += (_, e) =>
+                {
+                    if (!string.IsNullOrEmpty(e.Data))
+                        succeedOutput.AppendFormat(e.Data);
+                };
+                process.ErrorDataReceived += (_, e) =>
+                {
+                    if (!string.IsNullOrEmpty(e.Data))
+                        failedOutput.AppendFormat(e.Data.ToString());
+                };
+                process.EnableRaisingEvents = true;
+                process.StartInfo = startInfo;
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+                process.WaitForExit();
+            }
+
+            succeedLog = succeedOutput.ToString();
+            failedLog = failedOutput.ToString();
+        }
+
+        /// <summary>
+        /// 获取当前进程版本信息
+        /// </summary>
+        /// <returns>FileVersionInfo</returns>
+        public static FileVersionInfo GetCurrentVersion()
+        {
+            var fileName = Process.GetCurrentProcess().MainModule?.FileName ?? string.Empty;
+            return string.IsNullOrEmpty(fileName) ? null : FileVersionInfo.GetVersionInfo(fileName);
         }
     }
 }
