6bbd6b2e6e4977e4fa51b975de6821a15b89e0fa
YanZhiwei <Yan.Zhiwei@hotmail.com>
2022-02-21 23:47:23 +0800

    dev20220221 (#34)
    * dev20220221
    
    * update nuget version

---
+++ XiangJiang/Common/ThreadHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using XiangJiang.Core;
+
+namespace XiangJiang.Common
+{
+    public static class ThreadHelper
+    {
+        public static void Cancel(this Thread thread)
+        {
+            Checker.Begin().NotNull(thread, nameof(thread));
+            if (thread.ThreadState != ThreadState.WaitSleepJoin)
+                return;
+
+            thread.Interrupt();
+        }
+    }
+}
