59ea381d19d75f12d7e696bec8212e4d937eb1ab
YanZhiwei <Yan.Zhiwei@hotmail.com>
2022-03-01 00:25:01 +0800

    dev20220228 (#35)

---
+++ XiangJiang/Common/DataTableHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using XiangJiang.Core;
+
+namespace XiangJiang.Common
+{
+    public static class DataTableHelper
+    {
+        public static decimal CastDecimalThenSum(this DataTable table, string columnName)
+        {
+            CheckTableAndColumnName(table, columnName);
+            return table.IsEmpty()
+                ? default
+                : table.AsEnumerable().Sum(x => x[columnName] is DBNull ? 0 : GetValue<decimal>(x[columnName]));
+        }
+
+        private static void CheckTableAndColumnName(DataTable table, string columnName)
+        {
+            Checker.Begin().NotNull(table, nameof(table)).NotNullOrEmpty(columnName, nameof(columnName));
+        }
+
+        public static decimal CastIntThenSum(this DataTable table, string columnName)
+        {
+            CheckTableAndColumnName(table, columnName);
+            return table.IsEmpty() ? default(int) : table.AsEnumerable().Sum(x => x[columnName] is DBNull ? 0 : GetValue<int>(x[columnName]));
+        }
+
+        public static float CastFloatThenSum(this DataTable table, string columnName)
+        {
+            CheckTableAndColumnName(table, columnName);
+            return table.IsEmpty() ? default : table.AsEnumerable().Sum(x => x[columnName] is DBNull ? 0 : GetValue<float>(x[columnName]));
+        }
+
+        public static bool ContainColumn(this DataTable table, string columnName)
+        {
+            CheckTableAndColumnName(table, columnName);
+            var columns = table.Columns;
+            return columns.Contains(columnName);
+        }
+
+        public static bool IsEmpty(this DataTable table)
+        {
+            return table == null || table.Rows.Count == 0 || table.Columns.Count == 0;
+        }
+
+        private static T GetValue<T>(object value)
+        {
+            return (T) Convert.ChangeType(value, typeof(T));
+        }
+    }
+}
---
+++ XiangJiangTests/Common/DataTableHelperTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XiangJiang.Common;
+
+namespace XiangJiangTests.Common
+{
+    [TestClass()]
+    public class DataTableHelperTests
+    {
+        private DataTable _mockDataTable;
+        [TestInitialize]
+        public void Init()
+        {
+            _mockDataTable = new DataTable();
+            _mockDataTable.TableName = "EMP";
+            _mockDataTable.Columns.Add("Name", typeof(string));
+            _mockDataTable.Columns.Add("Salary", typeof(string));
+            _mockDataTable.Columns.Add("Commission", typeof(int));
+
+            var dr = _mockDataTable.NewRow();
+            dr["Name"] = "Arnold";
+            dr["Salary"] = "10000";
+            dr["Commission"] = 20;
+            _mockDataTable.Rows.Add(dr);
+            dr = _mockDataTable.NewRow();
+            dr["Name"] = "Arnold";
+            dr["Salary"] = "3000";
+            dr["Commission"] = 15;
+            _mockDataTable.Rows.Add(dr);
+            dr = _mockDataTable.NewRow();
+            dr["Name"] = "Molin";
+            dr["Salary"] = null;
+            dr["Commission"] = 15;
+            _mockDataTable.Rows.Add(dr);
+        }
+
+        [TestMethod()]
+        public void CastDecimalThenSumTest()
+        {
+            decimal actual = _mockDataTable.CastDecimalThenSum("Salary");
+            Assert.AreEqual(13000M, actual);
+        }
+    }
+}
