80a97764f0027a13b0201d76a76f4cee2337f244
YanZhiwei <Yan.Zhiwei@hotmail.com>
2022-03-02 23:26:18 +0800

    Private/dev202202 release (#36)
    * dev20220214 (#32)
    
    * dev20220214
    
    * update nuget version
    
    * dev20220218 (#33)
    
    * dev20220221 (#34)
    
    * dev20220221
    
    * update nuget version
    
    * dev20220228 (#35)
    
    * dev202202Release

---
+++ XiangJiang/Common/DirectoryHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using XiangJiang.Core;
+
+namespace XiangJiang.Common
+{
+    public sealed class DirectoryHelper
+    {
+        public static IEnumerable<string> GetFiles(string folder, bool includedChildFolder = true, params string[] exts)
+        {
+            Checker.Begin().NotNullOrEmpty(folder, nameof(folder)).CheckDirectoryExist(folder)
+                .NotNull(exts, nameof(exts));
+            return Directory
+                .EnumerateFiles(folder, "*.*",
+                    includedChildFolder ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly)
+                .Where(s => exts.Contains(Path.GetExtension(s), StringComparer.OrdinalIgnoreCase));
+        }
+    }
+}
